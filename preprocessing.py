d68480f91009f184f687d8100e11a52c58c73375
Andrew Lai <arjlai221@gmail.com>
2022-02-15 10:37:42 -0800

    data importing complete

---
+++ preprocessing.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import os
+
+"""
+
+XLE = Energy
+XLF = Financial Services
+XLRE = Real Estate
+XLP = Consumer Staples
+XLB = Basic Materials
+XLK = Technology
+XLV = Healthcare
+XLI = Industrial
+XLY = Consumer Discretionary
+XLU = Utilities
+XLC = Communication Services
+
+"""
+
+def importData():
+    outputFrame = pd.read_csv('Eth_2021.csv')[['Date', 'Change %']]
+    outputFrame.columns = ['Date', 'ETH']
+    print(outputFrame)
+    # iterate over files in
+    # that directory
+    for filename in os.listdir('Data'):
+        indicator = filename.split('_')[0]
+        tmpFrame = pd.read_csv(f'Data/{filename}')[['Date', 'Change %']]
+        tmpFrame.columns = ['Date', indicator]
+        # want to inner merge b/c markets are closed on weekends and some holidays
+        outputFrame = outputFrame.merge(tmpFrame, how='inner', on='Date')
+    
+    return outputFrame
+
+
+importData()
